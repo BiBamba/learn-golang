7d3cbec68ee94f144a46932bebbac5b19f99bcc4
Bi Bamba <maxime-tizie@hotmail.fr>
2022-07-29 12:16:48 -0500

    create first go code

---
+++ data_types_and_variables/firstcode/main.go
@@ -0,0 +1,7 @@
+package main
+import ("fmt")
+
+func main() {
+	var greeting string = "Welcome to my Golang journey"
+	fmt.Println(greeting)
+}
---
+++ data_types_and_variables/printing_variables/main.go
@@ -0,0 +1,14 @@
+package main
+import "fmt"
+
+func main() {
+	var name string = "Bedraan!"
+	var user string = "Maxime"
+	fmt.Println("Welcome to ", name, ", ",user)
+
+	/* Println allows to print on a new line */
+	var group string = "Bedraan!"
+	var lname string = "maxime"
+	fmt.Println(group)
+	fmt.Println(lname)
+}
