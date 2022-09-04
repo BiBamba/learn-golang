f3f685f705b8cd7b4b939855d62a1ded6599aa93
Bi Bamba <maxime-tizie@hotmail.fr>
2022-09-04 17:01:40 -0500

    add functions getStudent and deleteStudent

--- students/main.go
+++ students/main.go
@@ -24,11 +24,34 @@ func getStudents(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(students)
 }
 
+func getStudent(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for _, item := range students {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+}
+
+func deleteStudent(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range students {
+		if item.ID == params["id"] {
+			students = append(students[:index], students[index+1:]...)
+			break
+		}
+	}
+}
+
 func main() {
 	students = append(students, Student{ID: "1", Firstname: "Max", Lastname: "Tizie", Grade: 90, Major: "Computer Science"})
 	r := mux.NewRouter()
 	r.HandleFunc("/students", getStudents).Methods("GET")
-	//r.HandleFunc("/student/{id}", getStudent).Methods("GET")
+	r.HandleFunc("/students/{id}", getStudent).Methods("GET")
+	r.HandleFunc("/students/{id}", deleteStudent).Methods("DELETE")
 
 	fmt.Printf("Starting server on port 8282\n")
 	log.Fatal(http.ListenAndServe(":8282", r))
